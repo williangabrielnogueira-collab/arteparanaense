a0b2016024227b70cc79a21ed76cf9569653f4e9
williangabrielnogueira-collab <willian.gabriel.nogueira@escola.pr.gov.br>
2026-09-16 17:15:43 -0300

    coloquei alguns botoes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.foreach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtit);
+    function curtir(){const contador = botaoCurtir.querySelector("span");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true;}
+            else{
+    contador.textContent--;
+    curtiu = false;
+   }
+}
+});
